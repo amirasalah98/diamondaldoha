44851d59c6d95e9f6c53c039f14ad9c359f948bf
amirasalah98 <amira.salahhh98@gmail.com>
2022-08-09 13:46:50 -0700

    new

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-var showAboutDetails=document.getElementsByClassName("")
-showAbout(){
-
-}
+$('.nav-home').on('click',function(){
+    $('.home').removeClass("display")
+    if($('.about-us'&&'.recruitment'&&'.contact-us'&&'.our-services').not('.display')){
+        $('.about-us').addClass('display')
+        $('.recruitment').addClass('display')
+        $('.contact-us').addClass('display')
+        $('.our-services').addClass('display')
+    }
+})
+$('.nav-about').on('click',function(){
+    $('.about-us').removeClass("display")
+    if($('.home'&&'.recruitment'&&'.contact-us'&&'.our-services').not('.display')){
+        $('.home').addClass('display')
+        $('.recruitment').addClass('display')
+        $('.contact-us').addClass('display')
+        $('.our-services').addClass('display')
+    }
+})
+$('.nav-recruit').on('click',function(){
+    $('.recruitment').removeClass("display")
+    if($('.about-us'&&'.home'&&'.contact-us'&&'.our-services').not('.display')){
+        $('.about-us').addClass('display')
+        $('.home').addClass('display')
+        $('.contact-us').addClass('display')
+        $('.our-services').addClass('display')
+    }
+})
+$('.nav-services').on('click',function(){
+    $('.our-services').removeClass("display")
+    if($('.about-us'&&'.recruitment'&&'.contact-us'&&'.home').not('.display')){
+        $('.about-us').addClass('display')
+        $('.recruitment').addClass('display')
+        $('.contact-us').addClass('display')
+        $('.home').addClass('display')
+    }
+})
+$('.nav-contact').on('click',function(){
+    $('.contact-us').removeClass("display")
+    if($('.about-us'&&'.recruitment'&&'.home'&&'.our-services').not('.display')){
+        $('.about-us').addClass('display')
+        $('.recruitment').addClass('display')
+        $('.home').addClass('display')
+        $('.our-services').addClass('display')
+    }
+})
